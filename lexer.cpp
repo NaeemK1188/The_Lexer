47a7c43dbd99ab289f4009978fe71a8ea3e86bf5
SebastianRivera6 <Sebastian_river6@csu.fullerton.edu>
2023-10-03 21:10:01 -0700

    added readtext function and output to text file functionality

--- lexer.cpp
+++ lexer.cpp
@@ -4,16 +4,47 @@
 #include <cctype>
 #include <regex>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 
+string readtext(const string& filename) {
+    ifstream file(filename);
+    string textFile;
+
+    if (file.is_open()) {
+        // Read the entire file into the string
+        file.seekg(0, ios::end);
+        textFile.reserve(file.tellg());
+        file.seekg(0, ios::beg);
+
+        textFile.assign(
+            (istreambuf_iterator<char>(file)),
+            istreambuf_iterator<char>()
+        );
+
+        file.close();
+    } else {
+        cerr << "Unable to open file: " << filename << endl;
+    }
+
+    return textFile;
+}
 
 
 int main()
 {
+	ofstream outputFile("output.txt");
+
+  streambuf *original = std::cout.rdbuf();
+  cout.rdbuf(outputFile.rdbuf());
 
 
-	string sourceCode = "while (t < upper)s=22.00;";
-	string sourceCode1 = "int s=20; cout<<s<<endl;";
+	cout<< "Token      |Lexeme " << endl;
+	cout<< "-----------|------" << endl;
+
+
+	string filename = "input_scode.txt";
+  string fileContents = readtext(filename);
 	string keywords[] = { "cin", "cout", "while", "if", "else", "for", "int", "float", "endl" };
 	string regexId = "\\b(?!(cin|cout|while|for|if|else|int|float|endl)\\b)[a-zA-Z_]\\w*\\b";
 	string symbol = "[{}();,]| >> | <<";
@@ -24,40 +55,40 @@ int main()
 	regex regexOper(ope);
 	regex regexSy(symbol);
 	smatch match;
-	string::const_iterator  searchStart(sourceCode.cbegin());
-	
+	string::const_iterator  searchStart(fileContents.cbegin());
 
 
-	while (regex_search(searchStart, sourceCode.cend(), match, regexDigit)) // != eol()
+
+	while (regex_search(searchStart, fileContents.cend(), match, regexDigit)) // != eol()
 	{
 		//string matchDig = match[0];
 		//double number = stod(matchDig);
-		cout << "constant: " << match[0]; // matchDig
+		cout << "constant   |" << match[0]; // matchDig
 		searchStart = match.suffix().first; // i++
 	}
-	
+
 	cout << endl;
 
-	searchStart = sourceCode.cbegin(); // reset starting point to th beginning of the sourceCode string 
+	searchStart = fileContents.cbegin(); // reset starting point to th beginning of the fileContents string
 
 
-	while (regex_search(searchStart, sourceCode.cend(), match, regexIdentifier))
+	while (regex_search(searchStart, fileContents.cend(), match, regexIdentifier))
 	{
-		
-		
 
-		cout << "Identifier: " << match[0] << " ";
+
+
+		cout << "Identifier |" << match[0] << " ";
 		searchStart = match.suffix().first;
 		cout << endl;
-		
+
 	}
 
-	searchStart = sourceCode.cbegin();
+	searchStart = fileContents.cbegin();
 
-	while (regex_search(searchStart, sourceCode.cend(), match, regexOper))
+	while (regex_search(searchStart, fileContents.cend(), match, regexOper))
 	{
 
-		cout << "Operator: " << match[0] << " ";
+		cout << "Operator   |" << match[0] << " ";
 
 		searchStart = match.suffix().first;
 		cout << endl;
@@ -66,23 +97,24 @@ int main()
 
 	for (string keyword : keywords)
 	{
-		if (sourceCode.find(keyword) != string::npos)
+		if (fileContents.find(keyword) != string::npos)
 		{
-			cout << "keyword: " << keyword << endl;
+			cout << "keyword    |" << keyword << endl;
 		}
 	}
 
-	searchStart = sourceCode.cbegin();
+	searchStart = fileContents.cbegin();
 
-	while (regex_search(searchStart, sourceCode.cend(), match, regexSy))
+	while (regex_search(searchStart, fileContents.cend(), match, regexSy))
 	{
 
-		cout << "Symbol: " << match[0] << " ";
+		cout << "Symbol     |" << match[0] << " ";
 
 		searchStart = match.suffix().first;
 		cout << endl;
 	}
 
-    
+	std::cout.rdbuf(original);
+	outputFile.close();
 	return 0;
 }
